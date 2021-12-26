3d3205e5ca8bb8a6ed2a5619102e577b54fa29cb
minennai <ignaciominenna@gmail.com>
2021-12-26 19:12:16 -0300

    update index

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,24 @@ import * as React from 'react'
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 
-function App() {
-  // 2. Wrap ChakraProvider at the root of your app
-  return (
-    <ChakraProvider>
+const App = () => {
+    const loginHook = useLogin();
+    const graphHook = useGraph(loginHook);
+  
+    return (
+      <UserContext.Provider value={loginHook}>
+        <GraphContext.Provider value={graphHook}>
+          <MainApp />
+        </GraphContext.Provider>
+      </UserContext.Provider>
+    );
+  };
+  
+  ReactDOM.render(
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
       <App />
-    </ChakraProvider>
-  )
-}
+    </ChakraProvider>,
+    document.getElementById("root")
+  );
