5416ab2dbc590146aa13c4b250170e4c2d8c785d
minennai <ignaciominenna@gmail.com>
2021-12-26 19:57:27 -0300

    erased

--- src/data/index.js
+++
@@ -1,6 +0,0 @@
-export { administracion } from "./administracion";
-export { contador } from "./contador";
-export { sistemas } from "./sistemas";
-export { actuarioadmin } from "./actuarioadmin";
-export { actuarioeco } from "./actuarioeco";
-export { economia } from "./economia";
